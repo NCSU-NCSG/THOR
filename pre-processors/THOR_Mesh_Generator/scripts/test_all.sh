97aa34c40ad94ce320b16c50c3ceeae41a8d1805
Sebastian Schunert <snschune@ncsu.edu>
2018-07-25 13:18:57 -0600

    Conversion of old to new THOR mesh (#143)

--- pre-processors/THOR_Mesh_Generator/scripts/test_all.sh
+++ pre-processors/THOR_Mesh_Generator/scripts/test_all.sh
@@ -1,69 +1,122 @@
+message1=">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+message2="<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n"
+
+printf $message1
+echo Test 1: basic_cube_mesh_test
 cd ../test/basic_cube_mesh_test
   pwd
   bash ./run_test.sh
   cd -
+printf $message2
 
+printf $message1
+echo Test 2: homogeneous_domain
 cd ../test/homogeneous_domain
   pwd
   bash ./run_test.sh
   cd -
+printf $message2
 
+printf $message1
+echo Test 3: bad_invocation_no_-i
 cd ../test/bad_invocation_no_-i
   pwd
   bash ./run_test.sh
 cd -
+printf $message2
 
+printf $message1
+echo Test 4: bad_invocation_wrong_arg
 cd ../test/bad_invocation_wrong_arg
   pwd
   bash ./run_test.sh
 cd -
+printf $message2
 
+printf $message1
+echo Test 5: bad_invocation_no_input_file
 cd ../test/bad_invocation_no_input_file
   pwd
   bash ./run_test.sh
 cd -
+printf $message2
 
+printf $message1
+echo Test 6: bad_invocation_unsupported_file_type_unknown
 cd ../test/bad_invocation_unsupported_file_type_unknown
   pwd
   bash ./run_test.sh
 cd -
+printf $message2
 
+printf $message1
+echo Test 7: bad_invocation_unsupported_file_type_exodus
 cd ../test/bad_invocation_unsupported_file_type_exodus
   pwd
   bash ./run_test.sh
 cd -
+printf $message2
 
+printf $message1
+echo Test 8: bad_gmesh_non_tet_element
 cd ../test/bad_gmesh_non_tet_element
   pwd
   bash ./run_test.sh
 cd -
+printf $message2
 
+printf $message1
+echo Test 9: bad_gmesh_non_tri_face
 cd ../test/bad_gmesh_non_tri_face
   pwd
   bash ./run_test.sh
 cd -
+printf $message2
 
+printf $message1
+echo Test 10: bad_gmesh_no_elements_block
 cd ../test/bad_gmesh_no_elements_block
   pwd
   bash ./run_test.sh
 cd -
+printf $message2
 
+printf $message1
+echo Test 11: bad_gmesh_no_nodes_block
 cd ../test/bad_gmesh_no_nodes_block
   pwd
   bash ./run_test.sh
 cd -
+printf $message2
 
+printf $message1
+echo Test 12: bad_gmesh_no_format_block
 cd ../test/bad_gmesh_no_format_block
   pwd
   bash ./run_test.sh
 cd -
+printf $message2
 
+printf $message1
+echo Test 13: unv_sphere_in_shell_in_box
 cd ../test/unv_sphere_in_shell_in_box
   pwd
   bash ./run_test.sh
 cd -
+printf $message2
 
+printf $message1
+echo Test 14: bad_unv_sphere_in_shell_in_box_no_2411
 cd ../test/bad_unv_sphere_in_shell_in_box_no_2411
   pwd
   bash ./run_test.sh
 cd -
+printf $message2
+
+printf $message1
+echo Test 15: convert_old_to_new_THOR
+cd ../test/convert_old_to_new_THOR
+  pwd
+  bash ./run_test.sh
+cd -
+printf $message2
--- pre-processors/THOR_Mesh_Generator/src/thor_mesh.f95
+++ pre-processors/THOR_Mesh_Generator/src/thor_mesh.f95
@@ -22,7 +22,139 @@ CONTAINS
 
   !-----------------------------------------------------------------------------
   !-----------------------------------------------------------------------------
-  !> Extracts elements, node, bc, and block_id data from the gmesh file
+  !> Updates old to new THOR format
+  !-----------------------------------------------------------------------------
+  !-----------------------------------------------------------------------------
+  SUBROUTINE oldToNewTHORFormat()
+    INTEGER :: i
+
+    DO i = 1, bc_count
+      !Modify boundary IDs
+      side_set(i) = side_set(i) - 1_li
+
+      !Modify face id: this is necessary because the original
+      !format shuffled these in a weird way
+      IF (boundary_face_list(i) .eq. 1_li) THEN
+        boundary_face_list(i) = 2_li
+      ELSE IF (boundary_face_list(i) .eq. 2_li) THEN
+        boundary_face_list(i) = 0_li
+      ELSE IF (boundary_face_list(i) .eq. 3_li) THEN
+        boundary_face_list(i) = 1_li
+      ELSE IF (boundary_face_list(i) .eq. 4_li) THEN
+        boundary_face_list(i) = 3_li
+      END IF
+
+    END DO
+
+  END SUBROUTINE
+
+  !-----------------------------------------------------------------------------
+  !-----------------------------------------------------------------------------
+  !> Extracts the total number of sides from THOR mesh file
+  !-----------------------------------------------------------------------------
+  !-----------------------------------------------------------------------------
+  SUBROUTINE getTHORNumberSides(n_instructions, side_count)
+    INTEGER :: n_instructions, side_count
+    INTEGER :: n_node, n_elem, local_nside
+    INTEGER :: i, j
+
+    OPEN(UNIT = in_unit, file = in_file, ACTION = "read")
+
+    !Header
+    READ(in_unit, *) n_node
+    READ(in_unit, *) n_elem
+    READ(in_unit, *)
+    READ(in_unit, *) n_instructions
+
+    DO i = 1, n_node + 2 * n_elem
+      READ (in_unit, *)
+    END DO
+
+    DO i = 1, n_instructions
+      READ(in_unit, *) local_nside
+      side_count = side_count + local_nside
+      DO j = 1, local_nside
+        READ(in_unit, *)
+      END DO
+    END DO
+
+    CLOSE(in_unit)
+  END SUBROUTINE
+
+  !-----------------------------------------------------------------------------
+  !-----------------------------------------------------------------------------
+  !> Reads a THOR mesh file
+  !-----------------------------------------------------------------------------
+  !-----------------------------------------------------------------------------
+  SUBROUTINE ingestThorMesh()
+
+    INTEGER :: i, j, l, k, s
+    INTEGER :: number_side_instructions
+
+    !old THOR format allows more than one side instructions
+    !need to get the actual number of side faces
+    CALL getTHORNumberSides(number_side_instructions, bc_count)
+
+    !Open File
+    OPEN(UNIT = in_unit, file = in_file, ACTION = "read")
+
+    !Header
+    READ(in_unit, *) node_count
+    READ(in_unit, *) element_count
+    !TODO: Find out what these 1 entries are for:
+    READ(in_unit, *)
+    READ(in_unit, *)
+
+    !Allocate node, element, and block_id array
+    ALLOCATE(node_list(node_count,3),element_list(element_count,4),block_id(element_count),&
+             source_id(element_count))
+
+    !READ node_list
+    DO i = 1, node_count
+      READ (in_unit, *) j, node_list(i, :)
+    END DO
+
+    !Read block_id and source_id
+    DO i = 1, element_count
+      READ (in_unit, *) j, block_id(i), source_id(i)
+    END DO
+
+    !Read element_list
+    DO i = 1, element_count
+      READ (in_unit, *) j, element_list(i, :)
+    END DO
+
+    !Read bc
+    ALLOCATE(boundary_element_list(bc_count), boundary_face_list(bc_count),&
+             side_set(bc_count))
+    s = 1
+    DO i = 1, number_side_instructions
+      READ (in_unit, *) j
+      DO l = 1, j
+        READ (in_unit, *) boundary_element_list(s), boundary_face_list(s), side_set(s)
+        s = s + 1
+      END DO
+    END DO
+
+    !Read adjacency list information
+    READ (in_unit, *) j
+    ALLOCATE(adjacency_map(element_count,4))
+    DO i = 1, 4 * element_count
+      READ (in_unit, *) l, s, k, j
+      IF (k .eq. 0) THEN
+        adjacency_map(l, s + 1) = -1
+      ELSE
+        adjacency_map(l, s + 1) = k
+      END IF
+    END DO
+
+    CLOSE(in_unit)
+
+  END SUBROUTINE
+
+  !-----------------------------------------------------------------------------
+  !-----------------------------------------------------------------------------
+  !> Writes a THOR mesh file
   !-----------------------------------------------------------------------------
   !-----------------------------------------------------------------------------
   SUBROUTINE outputThorMesh()
@@ -66,43 +198,53 @@ CONTAINS
       WRITE(out_unit, '(I0, 4(X,I0))') i, element_list(i,:)
     END DO
 
-    ! If boundary conditions are determined from adjacency go into first branch
-    IF (boundary_condition_from_adjacency) THEN
-      WRITE(out_unit, '(I0)') SIZE(boundary_element_list)
-      DO j = 1, SIZE(boundary_element_list)
-        element = boundary_element_list(j)
-        current_face = boundary_face_list(j)
-        WRITE(out_unit, '(I0, 2(X,I0))') element, current_face, single_boundary_condition_type
+    !if we simply reformat the mesh, then just do a primitive printing of data
+    !no additional work is necessary
+    IF (execution_mode .eq. 4_li) THEN
+      WRITE(out_unit, '(I0)') bc_count
+      DO j = 1, bc_count
+        WRITE(out_unit, '(I0, 2(X,I0))')  boundary_element_list(j),  boundary_face_list(j),&
+                                          side_set(j)
       END DO
     ELSE
-      !# Of BC Definitions
-      WRITE(out_unit, '(I0)') bc_count
-
-      ! TODO: this search can be costly for many bcs; in this case we need to
-      ! come up with a better lookup [consider string-hashes]
-      IF (SIZE(boundary_element_list) .NE. bc_count) &
-            CALL generateErrorMessage(err_code_default, err_fatal, &
-            'bc count from gmesh file and adjacency list are inconsistent')
-
-      ! TODO: better algorithm
-      ! TODO: if no better algorithm at least make it a pretty function
-      DO i = 1, bc_count
-        local_node_list_gmesh = bc_list(i, :)
-        CALL quickSortInteger(local_node_list_gmesh, order_gmesh)
-        DO j = 1, bc_count
+      ! If boundary conditions are determined from adjacency go into first branch
+      IF (boundary_condition_from_adjacency) THEN
+        WRITE(out_unit, '(I0)') SIZE(boundary_element_list)
+        DO j = 1, SIZE(boundary_element_list)
           element = boundary_element_list(j)
           current_face = boundary_face_list(j)
-          local_node_list_adjacency = element_list(element, :)
-          local_node_list_adjacency(current_face + 1) = -1
-          CALL quickSortInteger(local_node_list_adjacency, order_adjacency)
-          IF (local_node_list_gmesh(1) .EQ. local_node_list_adjacency(2) .AND. &
-                local_node_list_gmesh(2) .EQ. local_node_list_adjacency(3) .AND. &
-                local_node_list_gmesh(3) .EQ. local_node_list_adjacency(4)) THEN
-            position = mapIndexOf(side_set(i), boundary_id_map(:, 1))
-            WRITE(out_unit, '(I0, 2(X,I0))') element, current_face, boundary_id_map(position, 2)
-          END IF
+          WRITE(out_unit, '(I0, 2(X,I0))') element, current_face, single_boundary_condition_type
         END DO
-      END DO
+      ELSE
+        !# Of BC Definitions
+        WRITE(out_unit, '(I0)') bc_count
+
+        ! TODO: this search can be costly for many bcs; in this case we need to
+        ! come up with a better lookup [consider string-hashes]
+        IF (SIZE(boundary_element_list) .NE. bc_count) &
+              CALL generateErrorMessage(err_code_default, err_fatal, &
+              'bc count from gmesh file and adjacency list are inconsistent')
+
+        ! TODO: better algorithm
+        ! TODO: if no better algorithm at least make it a pretty function
+        DO i = 1, bc_count
+          local_node_list_gmesh = bc_list(i, :)
+          CALL quickSortInteger(local_node_list_gmesh, order_gmesh)
+          DO j = 1, bc_count
+            element = boundary_element_list(j)
+            current_face = boundary_face_list(j)
+            local_node_list_adjacency = element_list(element, :)
+            local_node_list_adjacency(current_face + 1) = -1
+            CALL quickSortInteger(local_node_list_adjacency, order_adjacency)
+            IF (local_node_list_gmesh(1) .EQ. local_node_list_adjacency(2) .AND. &
+                  local_node_list_gmesh(2) .EQ. local_node_list_adjacency(3) .AND. &
+                  local_node_list_gmesh(3) .EQ. local_node_list_adjacency(4)) THEN
+              position = mapIndexOf(side_set(i), boundary_id_map(:, 1))
+              WRITE(out_unit, '(I0, 2(X,I0))') element, current_face, boundary_id_map(position, 2)
+            END IF
+          END DO
+        END DO
+      END IF
     END IF
 
     ! write adjacency list
