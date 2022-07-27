3080ba041afefd635ba0a48b27beb44c44b81ebc
nfherrin <nfherrin@ncsu.edu>
2022-07-27 16:50:31 -0400

    Added adjoint transposition. Not right yet it seems

--- THOR/src/adjoint_module.f90
+++ THOR/src/adjoint_module.f90
@@ -20,16 +20,30 @@ CONTAINS
   !
   !**********************************************************************
   SUBROUTINE transpose_xs
-    INTEGER :: m,g
+    INTEGER :: m,g,gp,j
     REAL(8),ALLOCATABLE :: temp_mat(:,:)
 
     ALLOCATE(temp_mat(egmax,egmax))
     !loop over all materials
     DO m=1,num_mat
-      DO g=1,egmax
+      !loop over angular moments for scattering
+      DO j=1,xs_ord+1
+        temp_mat=xs_mat(m)%sigma_scat(j,:,:)
+        !transpose the matrix
+        DO g=1,egmax
+          DO gp=1,egmax
+            xs_mat(m)%sigma_scat(j,g,gp)=temp_mat(gp,g)
+          ENDDO
+        ENDDO
       ENDDO
+      !swap nusigmaf and chi
+      temp_mat=0
+      temp_mat(1,:)=xs_mat(m)%chi(:)
+      temp_mat(2,:)=xs_mat(m)%nusig_f(:)
+      xs_mat(m)%chi(:)=temp_mat(2,:)
+      xs_mat(m)%nusig_f(:)=temp_mat(1,:)
     ENDDO
     DEALLOCATE(temp_mat)
-    stop 'transpose_xs not complete'
+    !stop 'transpose_xs not complete'
   ENDSUBROUTINE transpose_xs
 END MODULE adjoint_module
